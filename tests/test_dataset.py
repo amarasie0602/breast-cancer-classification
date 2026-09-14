b33ab8bac51921e23af16d832b2614c57c619387
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-09-14 13:33:40 +0530

    Add magnification filter test and pytest.ini

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -11,3 +11,9 @@ def test_dataset_getitem_returns_image_and_label(breakhis_root):
     image, label = ds[0]
     assert image.size == (8, 8)
     assert label in (0, 1)
+
+
+def test_magnification_filter_restricts_samples(breakhis_root):
+    ds = BreakHisDataset(breakhis_root, magnification=40)
+    assert len(ds) == 4
+    assert all(s["magnification"] == "40" for s in ds.samples)
