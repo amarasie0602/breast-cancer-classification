bc508b8945a88ea2daa3d185a94f6c0c79656758
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-09-16 18:35:08 +0530

    Add cached checkpoint loader for serving

---
+++ src/serving/model_loader.py
@@ -0,0 +1,14 @@
+"""Load a trained checkpoint for inference, cached across requests."""
+
+from functools import lru_cache
+
+from src.models.classifier import BreakHisClassifier
+from src.training.checkpoint import load_checkpoint
+
+
+@lru_cache(maxsize=4)
+def get_model(checkpoint_path, device="cpu"):
+    model = BreakHisClassifier(pretrained=False).to(device)
+    load_checkpoint(checkpoint_path, model)
+    model.eval()
+    return model
