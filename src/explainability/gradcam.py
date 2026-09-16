82a7459d96f17ffb9ca4ea669324ffffa772d192
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-09-16 18:14:11 +0530

    Add GradCAM: register forward/backward hooks on target layer

---
+++ src/explainability/gradcam.py
@@ -0,0 +1,20 @@
+"""Grad-CAM for visualizing which regions of a histology image drove a prediction."""
+
+
+class GradCAM:
+    """Registers forward/backward hooks on a target conv layer to compute Grad-CAM."""
+
+    def __init__(self, model, target_layer):
+        self.model = model
+        self.target_layer = target_layer
+        self.activations = None
+        self.gradients = None
+
+        target_layer.register_forward_hook(self._save_activations)
+        target_layer.register_full_backward_hook(self._save_gradients)
+
+    def _save_activations(self, module, input, output):
+        self.activations = output.detach()
+
+    def _save_gradients(self, module, grad_input, grad_output):
+        self.gradients = grad_output[0].detach()
