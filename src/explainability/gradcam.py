3352baa0d9ab37c92ba6d3c8e6ec33117e6e67f3
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-09-16 18:14:42 +0530

    Add GradCAM CAM computation: weighted activation sum, ReLU, normalize

--- src/explainability/gradcam.py
+++ src/explainability/gradcam.py
@@ -1,5 +1,8 @@
 """Grad-CAM for visualizing which regions of a histology image drove a prediction."""
 
+import torch
+import torch.nn.functional as F
+
 
 class GradCAM:
     """Registers forward/backward hooks on a target conv layer to compute Grad-CAM."""
@@ -18,3 +21,22 @@ class GradCAM:
 
     def _save_gradients(self, module, grad_input, grad_output):
         self.gradients = grad_output[0].detach()
+
+    def __call__(self, input_tensor, target_class=None):
+        self.model.zero_grad()
+        logits = self.model(input_tensor)
+
+        if target_class is None:
+            score = logits.squeeze(1) if logits.dim() > 1 else logits
+        else:
+            score = logits[:, target_class]
+        score.sum().backward()
+
+        weights = self.gradients.mean(dim=(2, 3), keepdim=True)
+        cam = F.relu((weights * self.activations).sum(dim=1))
+
+        cam = cam - cam.amin(dim=(1, 2), keepdim=True)
+        cam_max = cam.amax(dim=(1, 2), keepdim=True)
+        cam = cam / (cam_max + 1e-8)
+
+        return cam
