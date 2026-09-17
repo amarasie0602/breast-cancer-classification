2673e1739a32663f72c2835d81c0e7de9b80847e
amarasie0602 <eg255786@engug.ruh.ac.lk>
2026-09-17 17:35:31 +0530

    Add type hints to RequestLoggingMiddleware

--- src/serving/logging_middleware.py
+++ src/serving/logging_middleware.py
@@ -4,12 +4,14 @@ import logging
 import time
 
 from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import Response
 
 logger = logging.getLogger("serving.requests")
 
 
 class RequestLoggingMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request, call_next):
+    async def dispatch(self, request: Request, call_next) -> Response:
         start = time.perf_counter()
         response = await call_next(request)
         latency_ms = (time.perf_counter() - start) * 1000
